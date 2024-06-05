4eb26a355626638bd7fb92f057f6a104abfddaea
HgO <hgo@batato.be>
2024-06-05 10:57:37 +0200

    even more simplifications

--- conf/renew_cert.sh
+++ conf/renew_cert.sh
@@ -1,26 +1,22 @@
-#!/bin/bash
-
-set -e
+#!/bin/bash -e
 
 SCRIPT_DIR=$(dirname "$0")
 
-if [[ ! -f /etc/openvpn/keys/credentials ]]
-then
-  >&2 echo "ERROR: Cannot find credentials for Neutrinet VPN: /etc/openvpn/keys/credentials doesn't exist."
+OPENVPN_KEYS_DIR="/etc/openvpn/keys"
+OPENVPN_CREDENTIALS_FILE="/etc/openvpn/keys/credentials"
+
+if [[ ! -f "${OPENVPN_CREDENTIALS_FILE}" ]]; then
+  >&2 echo "ERROR: Cannot find credentials for Neutrinet VPN: ${OPENVPN_CREDENTIALS_FILE} doesn't exist."
   exit 1
 fi
 
-login=$(sed -n 1p "/etc/openvpn/keys/credentials")
-password=$(sed -n 2p "/etc/openvpn/keys/credentials")
+renew_dir=$(mktemp -d /tmp/renew_cert.XXXXX)
 
-renew_dir=$(mktemp -d "/tmp/renew-cert-XXX")
 renew_params="$@"
+/usr/bin/env python3 $SCRIPT_DIR/renew.py -C "${OPENVPN_CREDENTIALS_FILE}" -d "${renew_dir}" $renew_params
 
-/usr/bin/python3 "$SCRIPT_DIR/renew.py" "$login" -p "$password" -c "/etc/openvpn/keys/user.crt" -s "/etc/openvpn/keys/ca-server.crt" -d "$renew_dir" $renew_params
-
-if [[ ! -f $renew_dir/ca.crt || ! -f $renew_dir/client.crt || ! -f $renew_dir/client.key ]]
-then
-  rm -rf "$renew_dir"
+if [[ ! -f "${renew_dir}/ca.crt" || ! -f "${renew_dir}/client.crt" || ! -f "${renew_dir}/client.key" ]]; then
+  rm -rf "${renew_dir}"
   exit 0
 fi
 
@@ -29,19 +25,22 @@ echo "Backuping OpenVPN config"
 yunohost backup create -n "renew-cert_$(date +'%Y-%m-%d')" --apps vpnclient
 
 echo "Copying new certificates"
-cp "$renew_dir/ca.crt" "/etc/openvpn/keys/ca-server.crt"
-cp "$renew_dir/client.crt" "/etc/openvpn/keys/user.crt"
-cp "$renew_dir/client.key" "/etc/openvpn/keys/user.key"
+cp "${renew_dir}/ca.crt" "${OPENVPN_KEYS_DIR}/ca-server.crt"
+cp "${renew_dir}/client.crt" "${OPENVPN_KEYS_DIR}/user.crt"
+cp "${renew_dir}/client.key" "${OPENVPN_KEYS_DIR}/user.key"
+
+echo "Setting permissions"
+chmod 0600 "${OPENVPN_KEYS_DIR}/user.key"
+chmod 0600 "${OPENVPN_KEYS_DIR}/credentials"
 
 echo "Cleaning up files"
 rm -rf "$renew_dir"
 
-echo "Critical part: restarting VPNClient"
-systemctl restart ynh-vpnclient
+echo "Restarting VPN client to apply new certificate"
+yunohost service restart vpnclient
 
-if ! ynh-vpnclient status
-then
-  >&2 echo "ERROR: Failed to restart VPNClient"
+if ! ynh-vpnclient status; then
+  >&2 echo "ERROR: Failed to restart VPN client"
   tail -n 200 "/var/log/openvpn-client.log"
   exit 1
 fi
